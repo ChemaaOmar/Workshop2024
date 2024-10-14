76af4ebe880a9a72f5a6c6fb986df5bba3dab864
LBFK924 <162118942+LBFK924@users.noreply.github.com>
2024-10-14 16:34:27 +0200

    add firt page blabla

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// script.js
+$(document).ready(function(){
+    var categories = [
+        { name: "Facebook", icon: "facebook-icon.png" },
+        { name: "Instagram", icon: "instagram-icon.png" },
+        { name: "X", icon: "x-icon.png" },
+        { name: "TikTok", icon: "tiktok-icon.png" }
+    ];
+    var currentIndex = 0;
+
+    function updateIcons() {
+        $('.social-icons img').attr('src', categories[currentIndex].icon);
+    }
+
+    $('#prevBtn').click(function(){
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateIcons();
+        }
+        checkButtons();
+    });
+
+    $('#nextBtn').click(function(){
+        if (currentIndex < categories.length - 1) {
+            currentIndex++;
+            updateIcons();
+        }
+        checkButtons();
+    });
+
+    function checkButtons() {
+        if (currentIndex === 0) {
+            $('#prevBtn').attr('disabled', true);
+        } else {
+            $('#prevBtn').attr('disabled', false);
+        }
+
+        if (currentIndex === categories.length - 1) {
+            $('#nextBtn').attr('disabled', true);
+        } else {
+            $('#nextBtn').attr('disabled', false);
+        }
+    }
+
+    // Initial state
+    updateIcons();
+    checkButtons();
+});
